9b5f005d72bf8fef2a3b72a0340dd509133f8449
purpleice9765 <purpleice9765@msn.com>
2015-09-17 15:04:29 +0800

    new

--- app/controllers/travellers_controller.rb
+++ app/controllers/travellers_controller.rb
@@ -3,4 +3,27 @@ class TravellersController < ApplicationController
   		@travellers = Traveller.all
 	end
 
+
+	def new
+		@traveller = Traveller.new
+	end
+
+	def create
+  		@traveller = Traveller.new(traveller_params)
+  		@traveller.save
+
+  		redirect_to :action => :index
+	end
+
+	def show
+		@traveller = Traveller.find( params[:id])
+	end
+
+
+	private
+
+	def traveller_params
+  		params.require(:traveller).permit(:city, :date)
+	end
+
 end
