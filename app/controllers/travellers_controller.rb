cd2e8ba3ecde399e76222a6d5619216d52f5777d
purpleice9765 <purpleice9765@msn.com>
2015-09-17 15:32:13 +0800

    update

--- app/controllers/travellers_controller.rb
+++ app/controllers/travellers_controller.rb
@@ -20,9 +20,20 @@ class TravellersController < ApplicationController
 	end
 
 	def edit
-		@traveller=Traveller.find( params[:id])
-		
+		@traveller = Traveller.find( params[:id])
 	end
+
+	def update
+		@traveller = Traveller.find( params[:id])
+
+		if @traveller.update(traveller_params)
+      		redirect_to traveller_path(@traveller)
+      		flash[:notice] = "it was successfully updated"
+    	else
+      		render :action => :edit
+    	end
+	end
+
 
 
 	private
