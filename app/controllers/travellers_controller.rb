c94f814355526700fdd7ca3a2405f4c975ba006f
purpleice9765 <purpleice9765@msn.com>
2015-09-18 10:59:39 +0800

    add respond_to controller.rb的index Action加上XML、JSON ATOM的支援

--- app/controllers/travellers_controller.rb
+++ app/controllers/travellers_controller.rb
@@ -5,6 +5,14 @@ class TravellersController < ApplicationController
   		#@travellers = Traveller.page(params[:page]).per(5)
   		#如果要做 id 遞降
   		@travellers = Traveller.order("id DESC").page(params[:page]).per(5)
+	
+  		respond_to do |format|
+    		format.html # index.html.erb
+    		format.xml { render :xml => @events.to_xml }
+    		format.json { render :json => @events.to_json }
+    		format.atom { @feed_title = "My event list" } # index.atom.builder
+  		end
+
 	end
 
 
